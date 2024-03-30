7221699774e7cb49b9a8374cb04848f2e6c0d3d9
Ugam Raj <ugamrajseervi1994@gmail.com>
2024-03-30 21:57:03 +0530

    updatedb db

--- Ecommerce Application/index.js
+++ Ecommerce Application/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import DbConnect from "./Config/DBConnect.js";
+// import DbConnect from "./Config/DBConnect.js";
 import userAuthRoute from "./Routes/userRoute.js";
 import { errorHandler, notFound } from "./Middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
 import productRoute from "./Routes/productRoute.js";
 import morgan from "morgan";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
@@ -13,13 +14,22 @@ dotenv.config();
 const PORT = process.env.PORT || 10000;
 const DATABASE_URL = process.env.DATABASE_URL;
 const DB_NAME = process.env.DB_NAME;
+// console.log("🚀 ~ DATABASE_URL:", DATABASE_URL);
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
 //! Database connection
-DbConnect(DATABASE_URL, DB_NAME);
+// DbConnect(DATABASE_URL, DB_NAME);
+mongoose
+  .connect(DATABASE_URL, { dbName: DB_NAME })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log("🚀 ~ err:", err);
+  });
 
 app.use("/api/user", userAuthRoute);
 app.use("/api/product", productRoute);
